bf91db54b36dcfa904688a794bad2647064989c1
RIOKOWI <achyarrio2004@gmail.com>
2026-02-15 19:48:01 +0700

    feat: create logic at chat controller and create api key config

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -3,8 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ChatController extends Controller
 {
-    //
+    private $apiKey;
+
+    public function __construct()
+    {
+        $this->apiKey = config('gpt.api_key');
+    }
+
+    public function __invoke(Request $request)
+    {
+        $response = Http::withHeaders([
+            "Content-Type" => "application/json",
+            "Authorization" => "Bearer " . $this->apiKey
+        ])->post('https://api.openai.com/v1/chat/completions', [
+            "model" => "gpt-3.5-turbo",
+            "messages" => [
+                [
+                    "role" => "user",
+                    "content" => $request->post('content')
+                ]
+            ],
+            "temperature" => 0,
+            "max_tokens" => 2048
+        ])->body();
+
+        return response()->json(json_encode($response));
+    }
 }
